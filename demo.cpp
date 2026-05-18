8caf2e8ec3bcff8c014ddecf9b907835b02ed7c3
sana jalal <sanajalal654@gmail.com>
2026-05-18 14:50:23 +0500

    LAB 9 File Handling

---
+++ demo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    float gpa;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your GPA: ";
+    cin >> gpa;
+
+    cout << "\nStudent Information" << endl;
+    cout << "Name: sana jalal" << name << endl;
+    cout << "GPA:3.5 " << gpa << endl;
+
+    return 0;
+}
